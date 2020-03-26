cd4e89d921867016cf6af0dd34dfbb3c616b97f9
Tuan Dang <dangtony98@gmail.com>
2020-03-26 22:37:04 +0700

    First build

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     devtool: 'source-map',
-    mode: 'development',
+    mode: 'production',
     // entry: {
     //     polyfill: 'babel-polyfill',
     //     app: path.join(__dirname, '/src/app.js')
